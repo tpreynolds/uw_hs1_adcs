d585f36547ebe537faf5c4cc877b56a95a35f89c
Taylor Reynolds <taylorreynolds@Taylors-iMac.local>
2018-04-07 15:57:46 -0700

    autocoded new bdot after update to include tumble status

--- adcs/sw/components/adcs_bdot/matlab/lib/local/init_bdot_controller.m
+++ adcs/sw/components/adcs_bdot/matlab/lib/local/init_bdot_controller.m
@@ -1,12 +1,11 @@
 function bdot = init_bdot_controller( fsw_params )
-
 % ----------------------------------------------------------------------- %
 % Loads the parameters of the b-dot controller using predefined fsw_params.
 %   Any block added should be initialized and defined in here.
-
+%
 % UW HuskySat-1, ADCS Subsystem
-% Last Edited: T.Reynolds 9.23.17
-
+% T.Reynolds 4.7.18
+%
 % ----------------------------------------------------------------------- %
 
 % Initial conditions
@@ -22,16 +21,17 @@ bdot.ic.invalid_input   = [0 0 0]';
 % Library parameters
 
 % Make sure gains are negative
-bdot.gain_matrix = diag([- fsw_params.actuators.magnetorquer.max_dipole_x/1.5e-6,...
-                         - fsw_params.actuators.magnetorquer.max_dipole_y/1.5e-6,...
-                         - fsw_params.actuators.magnetorquer.max_dipole_z/1.7e-6]);
+bdot.gain_matrix = diag([...
+            - fsw_params.actuators.magnetorquer.max_dipole_x/1.5e-6,...
+            - fsw_params.actuators.magnetorquer.max_dipole_y/1.5e-6,...
+            - fsw_params.actuators.magnetorquer.max_dipole_z/1.7e-6]);
                                
-% NOTE: 3e-6 is the cut off value of \dot{B} below which we no longer want to be
-% saturating the torque rods.
+% NOTE: 3e-6 is the cut off value of \dot{B} below which we no longer want 
+% to be saturating the torque rods.
 
-% -----
-bdot.sample_time_s  = 1/10; %[s] - sampling at 10Hz
-bdot.digital_value  = 127; 
+bdot.sample_time_s  = 1/10; %[s] - sampling rate of the library
+
+% LPF coefficients
 bdot.cutoff_freq    = 2*pi*0.1; % [rad/s]
 bdot.continuous_lpf = tf([bdot.cutoff_freq],[1 bdot.cutoff_freq]);
 bdot.discrete_lpf   = c2d(bdot.continuous_lpf,bdot.sample_time_s);
@@ -40,12 +40,27 @@ bdot.discrete_lpf   = c2d(bdot.continuous_lpf,bdot.sample_time_s);
 bdot.filter_num     = bdot.filter_num(2);
 bdot.filter_den     = bdot.filter_den(2);
 
-bdot.dv_2_m_X   = fsw_params.actuators.magnetorquer.max_dipole_x/bdot.digital_value;
+% Conversions
+bdot.digital_value  = 127; 
+bdot.dv_2_m_X   = fsw_params.actuators.magnetorquer.max_dipole_x/...
+                    bdot.digital_value;
 bdot.m_2_dv_X   = 1/bdot.dv_2_m_X;
 
-bdot.dv_2_m_Y   = fsw_params.actuators.magnetorquer.max_dipole_y/bdot.digital_value;
+bdot.dv_2_m_Y   = fsw_params.actuators.magnetorquer.max_dipole_y/...
+                    bdot.digital_value;
 bdot.m_2_dv_Y   = 1/bdot.dv_2_m_Y;
 
-bdot.dv_2_m_Z   = fsw_params.actuators.magnetorquer.max_dipole_z/bdot.digital_value;
+bdot.dv_2_m_Z   = fsw_params.actuators.magnetorquer.max_dipole_z/...
+                    bdot.digital_value;
 bdot.m_2_dv_Z   = 1/bdot.dv_2_m_Z;
+ 
+% Tumbling status flag
+bdot.B_min                  = 35e-6; % roughly min B over orbit [ T ]
+bdot.omega_radps_thresh     = fsw_params.bus.omega_radps_thresh;
+bdot.bdot_thresh.max        = ((sqrt(2)/2) * bdot.B_min) * ...
+                                bdot.omega_radps_thresh.max;
+bdot.bdot_thresh.min        = ((sqrt(2)/2) * bdot.B_min) * ...
+                                bdot.omega_radps_thresh.min;
+                            
+end
 
