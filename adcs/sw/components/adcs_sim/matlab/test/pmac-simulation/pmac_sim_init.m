30a97825f403d6595bd5d5bffd5e7d64cf1762c3
Bijan <barzb@uw.edu>
2018-04-13 17:04:50 -0700

    created simulation environment for pmac design, added LS approx of earth mag field

---
+++ adcs/sw/components/adcs_sim/matlab/test/pmac-simulation/pmac_sim_init.m
@@ -0,0 +1,18 @@
+clear variables; close all; clc;
+addpath(genpath(pwd))
+addpath(genpath('../../../../adcs_fsw/matlab/'))
+addpath(genpath('../../../../adcs_sim/matlab/include'))
+addpath(genpath('../../../../adcs_sim/matlab/tools'))
+addpath(genpath('../../../../adcs_sim/matlab/lib/local/actuator'))
+addpath(genpath('../../../../adcs_sim/matlab/lib/local/dynamics'))
+addpath(genpath('../../../../adcs_sim/matlab/lib/local/sensor/gps'))
+
+% Load bus stub definitions
+load('bus_definitions.mat')
+load('bus_definitions_fsw.mat')
+
+% Load parameters for both flight software and simulation
+fsw_params              = init_fsw_params();
+[sim_params,fsw_params] = init_sim_params(fsw_params);
+fsw_params.bdot         = init_bdot_controller(fsw_params);
+
