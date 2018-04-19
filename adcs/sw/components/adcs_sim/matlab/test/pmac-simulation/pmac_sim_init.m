a5a9ba8321cbb69fe33f383860c0786e3639d4c5
Bijan <barzb@uw.edu>
2018-04-19 12:45:08 -0700

    Init file setup for iterative design

--- adcs/sw/components/adcs_sim/matlab/test/pmac-simulation/pmac_sim_init.m
+++ adcs/sw/components/adcs_sim/matlab/test/pmac-simulation/pmac_sim_init.m
@@ -14,5 +14,54 @@ load('bus_definitions_fsw.mat')
 % Load parameters for both flight software and simulation
 fsw_params              = init_fsw_params();
 [sim_params,fsw_params] = init_sim_params(fsw_params);
-% fsw_params.bdot         = init_bdot_controller(fsw_params);
+
+% ------ Overrides ------ %
+
+% % Magnet dipole & alignment
+% Ta = 8e-8; Tg = 6e-8; Tr = 1e-8; % Environmental torques, aero, gravity, radiometric
+Trms = 1e-7; % from gerhardt, calculate rms for our satellite for more accuracy
+Bmin = 2e-5; % minimum field strength at 600km (Tesla)
+beta_max = deg2rad(10); % desired pointing accuracy
+m_min = 10*Trms/(Bmin*sin(beta_max));
+
+align_long = [0;0;1];
+align_wide = [1;0;0];
+
+m = linspace(m_min,2*m_min,10);
+align = [linspace(align_long(1),align_wide(1),10);linspace(align_long(2),align_wide(2),10);linspace(align_long(3),align_wide(3),10)];
+% alignment vector has to be unit vector:
+for i = 1:length(align(1,:))
+    align(:,i) = align(:,i)/norm(align(:,i));
+end
+
+% Hysteresis Rods
+sim_params.actuators.pmac.hyst
+
+% Simulation iterations
+I = length(m);
+J = length(align(1,:));
+
+
+% Simulations
+model = 'pmac_sim';
+load_system(model)
+for ii = 1:I
+    sim_params.actuators.pmac.magnet.m = m(ii);
+    for jj = 1:J
+        sim_params.actuators.pmac.magnet.align = align(:,jj);
+        sim_params.actuators.pmac.magnet.dipole = m*align;
+        
+        
+        
+        
+        % Run sim
+       simOut = sim(model); 
+    end
+end
+
+
+% Evaluation
+
+
+
 
