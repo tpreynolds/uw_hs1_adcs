70eeb3928176dc0f158836fa5fcc8c3aac00d102
Emma Hansen <emmah0@uw.edu>
2018-04-28 12:47:08 -0700

    Updated test init file for data processing

--- adcs/sw/components/adcs_fsw/matlab/test/mag_pd_control/mag_pd_control_test_init.m
+++ adcs/sw/components/adcs_fsw/matlab/test/mag_pd_control/mag_pd_control_test_init.m
@@ -5,7 +5,7 @@
 % choose the control gains. Uses either a random initial condition or
 % identity quaternion, and commands a given slewing maneuver.
 
-% Test 2: Recreating results from Silani and Lovera 2005.
+% Test 2: Recreating results from XXXXXX.
 
 % UW HuskySat-1, ADCS Subsystem
 %  Last Update: T. Reynolds 3.29.18
@@ -22,6 +22,9 @@ if run_test == 1
 
 % Start fresh
 run('sim_init.m')
+
+% Set sim time
+t_end   = 40000;
 
 % Overrides
 
@@ -47,9 +50,6 @@ run('sim_init.m')
 % quat_cmd    = [1/2;1/2;1/2;1/2];
 quat_cmd    = [1;0;0;0];
 omega_cmd   = zeros(3,1);
-
-% Set sim time
-t_end   = 360;
 
 % turn off mag noise
 sim_params.sensors.magnetometer.noise = 0;
@@ -96,70 +96,68 @@ sim_params.dynamics.ic.rate_init = 1e-1*[0.1; 0.1; 0.1];
 fsw_params.control.mag_pd_controller.p_gain = -diag([-0.03  -0.03  -0.15]);
 fsw_params.control.mag_pd_controller.d_gain = -diag([-21  -21  -35]);
 
-omega_cmd   = zeros(3,1);
-
 % -----
 
 % Simulation parameters
 run_time    = num2str(t_end);
-mdl         = 'mag_pd_control_test';
+mdl         = 'mag_pd_control_test_new';
 load_system(mdl);
 set_param(mdl, 'StopTime', run_time);
-%sim(mdl);
-
+sim(mdl);
+%%
 % ----- Analyze Results ----- %
 
-% quat    = logsout.getElement('<quaternion>').Values.Data;
-% omega   = logsout.getElement('<body_rates>').Values.Data;
-% cmdu       = logsout.getElement('cmd_torque').Values.Data;
-% cmdtime    = logsout.getElement('cmd_torque').Values.Time;
-% realu       = logsout.getElement('torque').Values.Data;
-% real_time   = logsout.getElement('torque').Values.Time;
-% cmdRPM     = squeeze(logsout.getElement('cmd_rpm').Values.Data);
-% realRPM     = logsout.getElement('real_RPM').Values.Data;
+quat        = logsout.getElement('<quaternion>').Values.Data;
+omega       = logsout.getElement('<body_rates_radps>').Values.Data;
+cmd_DV      = logsout.getElement('cmd_DV').Values.Data;
+cmd_time    = logsout.getElement('cmd_DV').Values.Time;
+real_dp     = logsout.getElement('dipole').Values.Data;
+real_time   = logsout.getElement('dipole').Values.Time;
+eul         = radtodeg(quat2eul(quat));
+cmd_dp      = [fsw_params.control.cmd_processing.dv_2_m_X fsw_params.control.cmd_processing.dv_2_m_Y fsw_params.control.cmd_processing.dv_2_m_Z].*double(cmd_DV);
 
-% q_d     = fsw_params.bus.quat_commanded;
-% diff    = zeros(1,length(tout));
-% for i = 1:length(tout)
-%     q_diff  = quatmultiply(quatconj(q_d'),quat(i,:));
-%     diff(i) = norm( q_diff(2:4) ) ;
-% end
-
+q_d         = quat_cmd; %fsw_params.bus.quat_commanded;
+diff        = zeros(1,length(tout));
+for i = 1:length(tout)
+    q_diff  = quatmultiply(quatconj(q_d'),quat(i,:));
+    diff(i) = norm( q_diff(2:4) ) ;
+end
 
 % ----- End Analysis ----- %
 % % Actual State Values
-% figure(1)
-% subplot(2,1,1)
-% plot(tout,quat)
-% title('Quaternion','FontSize',15)
-% subplot(2,1,2)
-% plot(tout,omega)
-% title('Angular Velocity [rad/s]','FontSize',15)
-% xlabel('Time [s]','FontSize',12)
-% 
-% % Commanded versus Applied Control Signals
-% figure(2)
-% subplot(2,2,1)
-% plot(cmdtime,cmdu)
-% title('Commanded Torque [Nm]','FontSize',15)
-% subplot(2,2,2)
-% plot(cmdtime,cmdRPM)
-% title('Commanded RW RPM','FontSize',15)
-% xlabel('Time [s]','FontSize',12)
-% subplot(2,2,3)
-% plot(real_time,realu)
-% title('Actual Torque [Nm]','FontSize',15)
-% subplot(2,2,4)
-% plot(tout,realRPM)
-% title('Actual RW RPM','FontSize',15)
-% xlabel('Time [s]','FontSize',12)
+figure(1)
+subplot(2,1,1)
+plot(tout,quat)
+title('Quaternion','FontSize',15)
+subplot(2,1,2)
+plot(tout,omega)
+title('Angular Velocity [rad/s]','FontSize',15)
+xlabel('Time [s]','FontSize',12)
+
+% Commanded versus Applied Control Signals
+figure(2)
+subplot(2,1,1)
+plot(cmd_time,cmd_dp)
+title('Commanded Dipole [Nm]','FontSize',15)
+subplot(2,1,2)
+plot(real_time,real_dp)
+title('Actual Dipole [Nm]','FontSize',15)
 
 % Attitude Error 
-% figure(3), hold on
-% plot(tout,diff,'LineWidth',1)
-% plot(tout,0.02*ones(1,length(tout)),'k--')
-% %plot([ts ts],[0 1],'k--')
-% xlabel('Time [s]','FontSize',12)
+figure(3), hold on
+plot(tout,diff,'LineWidth',1)
+plot(tout,0.02*ones(1,length(tout)),'k--')
+%plot([ts ts],[0 1],'k--')
+xlabel('Time [s]','FontSize',12)
+title('Error')
+
+% Euler Angles
+figure(4)
+plot(tout,eul(:,1),tout,eul(:,2),tout,eul(:,3))
+xlabel('Time [s]','FontSize',12)
+legend('Z','Y','X')
+title('Euler Angles')
+
 
 %save('workspace-test-NAME.mat')
 
@@ -213,11 +211,63 @@ fsw_params.control.cmd_processing.m_2_dv_Z   = 1/fsw_params.control.cmd_processi
 
 % Simulation parameters
 run_time    = num2str(t_end);
-mdl         = 'mag_pd_control_test';
+mdl         = 'mag_pd_control_test_new';
 load_system(mdl);
 set_param(mdl, 'StopTime', run_time);
 % sim(mdl);
 
+% ----- Analyze Results ----- %
+
+quat        = logsout.getElement('<quaternion>').Values.Data;
+omega       = logsout.getElement('<body_rates_radps>').Values.Data;
+cmd_DV      = logsout.getElement('cmd_DV').Values.Data;
+cmd_time    = logsout.getElement('cmd_DV').Values.Time;
+real_dp     = logsout.getElement('dipole').Values.Data;
+real_time   = logsout.getElement('dipole').Values.Time;
+eul         = radtodeg(quat2eul(quat));
+cmd_dp      = [fsw_params.control.cmd_processing.dv_2_m_X fsw_params.control.cmd_processing.dv_2_m_Y fsw_params.control.cmd_processing.dv_2_m_Z].*double(cmd_DV);
+
+q_d         = quat_cmd; %fsw_params.bus.quat_commanded;
+diff        = zeros(1,length(tout));
+for i = 1:length(tout)
+    q_diff  = quatmultiply(quatconj(q_d'),quat(i,:));
+    diff(i) = norm( q_diff(2:4) ) ;
+end
+
+% ----- End Analysis ----- %
+% % Actual State Values
+figure(1)
+subplot(2,1,1)
+plot(tout,quat)
+title('Quaternion','FontSize',15)
+subplot(2,1,2)
+plot(tout,omega)
+title('Angular Velocity [rad/s]','FontSize',15)
+xlabel('Time [s]','FontSize',12)
+
+% Commanded versus Applied Control Signals
+figure(2)
+subplot(2,1,1)
+plot(cmd_time,cmd_dp)
+title('Commanded Dipole [Nm]','FontSize',15)
+subplot(2,1,2)
+plot(real_time,real_dp)
+title('Actual Dipole [Nm]','FontSize',15)
+
+% Attitude Error 
+figure(3), hold on
+plot(tout,diff,'LineWidth',1)
+plot(tout,0.02*ones(1,length(tout)),'k--')
+%plot([ts ts],[0 1],'k--')
+xlabel('Time [s]','FontSize',12)
+title('Error')
+
+% Euler Angles
+figure(4)
+plot(tout,eul(:,1),tout,eul(:,2),tout,eul(:,3))
+xlabel('Time [s]','FontSize',12)
+legend('Z','Y','X')
+title('Euler Angles')
 
 %save('workspace-test-NAME.mat')
 end
