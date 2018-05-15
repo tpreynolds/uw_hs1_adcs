6925c2faa786635be371ccbef6997358a65aec10
Bijan <barzb@uw.edu>
2018-05-15 12:53:26 -0700

    Full version of Seattle pointing vector determination

---
+++ adcs/sw/components/adcs_fsw/matlab/tools/getSeattleNADIR.m
@@ -0,0 +1,40 @@
+% function r_SEA_eci = getSeattleNADIR()
+% Run simInit.m first
+
+gps_time = [154818;
+            2021];
+
+sample_time_s = 0.1;
+
+orbit_tle = [18;
+            6848.29166666977;
+            3.2923e-05;
+            98.5033;
+            67.1301;
+            0.0008911;
+            245.3514;
+            2.1593;
+            14.56154823];
+
+SEA_Lat = 47;
+
+% Set sim time
+t_end   = 6000;
+
+run_time    = num2str(t_end);
+mdl         = 'Seattle_NADIR_pointing';
+load_system(mdl);
+set_param(mdl, 'StopTime', run_time);
+sim(mdl);
+
+Origin2SEA  = logsout.getElement('Origin2SEA').Values.Data;
+
+R_SEA_eci = unique(Origin2SEA,'rows');
+[n,~]     = size(R_SEA_eci);
+
+for i = 1:n
+    if norm(R_SEA_eci(i,:)) == 0
+        continue
+    end
+    r_SEA_eci(i,:) = R_SEA_eci(i,:)/norm(R_SEA_eci(i,:));
+end
