bb2df55f68ed5a1e2edccbf877ab51eaf4a2f7ae
Taylor Patrick Reynolds <tpr6@uw.edu>
2018-05-23 19:55:02 -0700

    created a week in the life script to run long duration tests

--- adcs/sw/components/adcs_sim/matlab/lib/local/actuator/ppt/init_ppt.m
+++ adcs/sw/components/adcs_sim/matlab/lib/local/actuator/ppt/init_ppt.m
@@ -7,8 +7,9 @@ function [ ppt ] = init_ppt( fsw_params )
 %   Last Edited: T. Reynolds, 9.29.17
 % ----------------------------------------------------------------------- %
 
-ppt.t_charge    = 1.2; % time to charge in [ s ]
-ppt.t_fire      = 0.002; % time it takes to "fire" [ s ]
+ppt.t_charge    = 1.2;      % time to charge in [ s ]
+ppt.t_fire      = 0.002;    % time it takes to "fire" [ s ]
+ppt.t_on_pct  = 100 * (ppt.t_charge / fsw_params.bus.sync_pulse);
 
 ppt.sync_pulse      = fsw_params.bus.sync_pulse;
 ppt.t_charge_pct    = ppt.t_charge/ppt.sync_pulse;
